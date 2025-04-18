83fb839771f9725693803ec94e4151a5930e6dd6
Víctor Rafael Zambrano Sarabia <donvito4571@gmail.com>
2025-04-18 17:28:09 -0400

    Finish stuff

--- src/api.js
+++ src/api.js
@@ -1,5 +1,15 @@
 const API_BASE = "http://localhost:3000";
 
+export async function getStatus(token) {
+  const headers = { headers: { Authorization: `Bearer ${token}` } };
+  const response = await fetch(`${API_BASE}/status`, headers);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch drinks");
+  }
+  return await response.json();
+}
+
 export async function getDrinks(token) {
   const headers = { headers: { Authorization: `Bearer ${token}` } };
   const response = await fetch(`${API_BASE}/drinks`, headers);
@@ -10,6 +20,28 @@ export async function getDrinks(token) {
   return await response.json();
 }
 
+export async function takeDrink(drinkId) {
+  const headers = {
+    "Content-Type": "application/json",
+    headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+  };
+  const body = {drink_id: {drinkId}}
+  const response = await fetch(`${API_BASE}/user_drinks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: body,
+  });
+
+  if (!response.ok) {
+    throw new Error("Drink creation failed");
+  }
+
+  return await response.json();
+}
+
+// POST /user_drinks(.:format) user_drinks#create
 export async function signup(userData) {
   const response = await fetch(`${API_BASE}/users`, {
     method: "POST",
